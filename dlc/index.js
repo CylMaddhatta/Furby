34597ace0a800ce86e18fd034dd7ad7942c78231
CylMaddhatta <98507704+CylMaddhatta@users.noreply.github.com>
2022-01-31 19:14:59 -0500

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,54 +1,32 @@
 [
 { 
+        "file": "SciXMin.dlc", 
+        "title": "Science No Minify",
+        "buttons": [
+            { "title": "Science!", "action": [75,0,0,2] }
+        ]
+    },
+        { 
+        "file": "SciMin.dlc", 
+        "title": "Science Minify",
+        "buttons": [
+            { "title": "Science!", "action": [75,0,0,2] }
+        ]
+    },
+        { 
+        "file": "SciBin.dlc", 
+        "title": "Science Binary",
+        "buttons": [
+            { "title": "Science!", "action": [75,0,2,6] }
+        ]
+    },
+        { 
         "file": "SciV4.dlc", 
         "title": "Sci4",
         "buttons": [
             { "title": "Logo", "action": [75,0,0,2] }
         ]
     },
-        
-        { 
-        "file": "SciV3.dlc", 
-        "title": "Sci3",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,2,6] }
-        ]
-    },
- { 
-        "file": "SciV2.dlc", 
-        "title": "Science2",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,0,1] }
-        ]
-    },
-    { 
-        "file": "EvilSeed.dlc", 
-        "title": "Evil",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,0,1] }
-        ]
-    },
-    { 
-        "file": "Evilv2.dlc", 
-        "title": "Evil3",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,0,1] }
-        ]
-    },
-    {
-        "file": "Evilv3.dlc", 
-        "title": "Evil3",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,0,1] }
-        ]
-    },
-    { 
-        "file": "EvilV4.dlc", 
-        "title": "Evil4",
-        "buttons": [
-            { "title": "Logo", "action": [75,0,0,1] }
-        ]
-    }, 
     {
         "file": "chilli.dlc", 
         "title": "Chilli",
