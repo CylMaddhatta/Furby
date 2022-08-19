fe954f41ab3ca6792a190a128ddd3799fd4e0ea2
CylMaddhatta <98507704+CylMaddhatta@users.noreply.github.com>
2022-08-19 18:20:54 -0400

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,5 +1,5 @@
 [
-         { 
+    { 
         "file": "Void.dlc", 
         "title": "WAAAAAGH",
         "buttons": [
@@ -7,7 +7,7 @@
             { "title": "Void!", "action": [75,0,0,3] }
         ]
     },
-        { 
+    { 
         "file": "Waagh.dlc", 
         "title": "WAAAAAGH",
         "buttons": [
@@ -15,22 +15,21 @@
             { "title": "Void!", "action": [75,0,0,3] }
         ]
     },
-         { 
+    {
         "file": "EvilSeed.dlc", 
         "title": "Evil Seed",
         "buttons": [
             { "title": "What have you done?", "action": [75,0,0,2] }
         ]
     },
-        { 
+    { 
         "file": "SciFix.dlc", 
         "title": "Science!",
         "buttons": [
             { "title": "FUCK MY HOUSE!", "action": [75,0,0,2] }
-        ]
-    },      
-  
-         {
+         ]
+    },
+    {
         "file": "chilli.dlc", 
         "title": "Chilli",
         "buttons": [
