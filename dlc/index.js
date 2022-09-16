26a56bcabe76b3ac6f19b95bbaef75e0b0fc84c3
CylMaddhatta <98507704+CylMaddhatta@users.noreply.github.com>
2022-09-16 15:16:17 -0400

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -17,6 +17,7 @@
         "file": "Greenfix.dlc", 
         "title": "Greenfix",
         "buttons": [
+            { "title": "Green", "action": [75,0,0,1] }
             { "title": "Green 2", "action": [75,0,0,2] }
         ]
     },
