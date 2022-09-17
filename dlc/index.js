95238e09e17e05848f423dcd7df2ce01e86bea70
CylMaddhatta <98507704+CylMaddhatta@users.noreply.github.com>
2022-09-17 00:20:49 -0400

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,18 +1,18 @@
 [
     {
         "file": "Orks.dlc", 
-        "title": "Urk Kuldar",
+        "title": "Urk Kuldar1",
         "buttons": [
-            { "title": "Be Green", "action": [75,0,0,3] },
+            { "title": "Be Green", "action": [75,0,0,6] },
             { "title": "Waaagh", "action": [75,0,2,7] },
             { "title": "Boss", "action": [75,0,4,7] },
             { "title": "Pee", "action": [75,0,6,5] },
             { "title": "Listen", "action": [75,0,5,6] },
             { "title": "Spit", "action": [75,0,6,6] },
-            { "title": "Snotling", "action": [75,0,0,5] },
+            { "title": "Snotling", "action": [75,0,0,3] },
             { "title": "Top Ork", "action": [75,0,4,6] },
             { "title": "Snivel", "action": [75,0,7,6] },
-            { "title": "In Charge", "action": [75,0,1,1] }
+            { "title": "In Charge", "action": [75,0,1,2] }
         ]
     },
     {
